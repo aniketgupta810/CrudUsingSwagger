f216df648c7ba3dae3d13e4db16b8dcfe2c99f9c
ANIKET KUMAR <98165185+aniketgupta810@users.noreply.github.com>
2023-04-23 11:10:24 +0530

    initial Commit

---
+++ CRUD_USING_SWAGGER/Data/ContactsAPIDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace CRUD_USING_SWAGGER.Data
+{
+    public class ContactsAPIDbContext : DbContext
+
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Models.Contact> Contacts{ get; set; }
+    }
+}
---
+++ CRUD_USING_SWAGGER/Migrations/20230423051755_Add-Migration InitialMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_USING_SWAGGER.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigrationInitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
